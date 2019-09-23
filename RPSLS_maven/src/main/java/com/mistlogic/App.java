52c9bf83172bbf83478a77e408d457a34d1b2966
Ishan Madan <ishan@mistlogic.com>
2019-09-23 14:59:12 -0700

    added if/else if/else instead of switch, score report

--- RPSLS_maven/src/main/java/com/mistlogic/App.java
+++ RPSLS_maven/src/main/java/com/mistlogic/App.java
@@ -9,6 +9,9 @@ import java.util.concurrent.TimeUnit;
  */
 public class App 
 {
+    static int userScore = 0;
+    static int computerScore = 0;
+    
     public static void main(String[] args) throws InterruptedException {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
@@ -74,6 +77,19 @@ public class App
             System.out.println("I choose " + computerChoice + ".");
             String victory = victoryCheck(computerChoice, userChoice);
             System.out.println(victory);
+            
+            System.out.println();
+            
+            System.out.print("Score: " + userScore + " you, " + computerScore + " me. ");
+            if (userScore > computerScore) {
+                System.out.print("You're winning!");
+            } else if (userScore < computerScore) {
+                System.out.print("I'm winning!");
+            } else {
+                System.out.print("We're tied!");
+            }
+            
+            System.out.println();
 
             // wait
             TimeUnit.SECONDS.sleep(1);
@@ -93,75 +109,92 @@ public class App
         if (computerChoice.equals(userChoice)) {
             return "It's a tie!";
         }
-        switch(userChoice) {
-            case "rock":
-                switch(computerChoice) {
-                    case "paper":
-                        return "You lose! Paper covers rock.";
-                    case "scissors":
-                        return "You win! Rock crushes scissors.";
-                    case "lizard":
-                        return "You win! Rock crushes lizard.";
-                    case "spock":
-                        return "You lose! Spock vaporizes rock.";
-                    default:
-                        exit();
-                }
-            case "paper":
-                switch(computerChoice) {
-                    case "rock":
-                        return "You win! Paper covers rock.";
-                    case "scissors":
-                        return "You lose! Scissors cuts paper.";
-                    case "lizard":
-                        return "You lose! Lizard eats paper.";
-                    case "spock":
-                        return "You win! Paper disproves Spock.";
-                    default:
-                        exit();
-                }
-            case "scissors":
-                switch(computerChoice) {
-                    case "rock":
-                        return "You lose! Rock crushes scissors.";
-                    case "paper":
-                        return "You win! Scissors cuts paper.";
-                    case "lizard":
-                        return "You win! Scissors decapitates lizard.";
-                    case "spock":
-                        return "You lose! Spock smashes scissors.";
-                    default:
-                        exit();
-                }
-            case "lizard":
-                switch(computerChoice) {
-                    case "rock":
-                        return "You lose! Rock crushes lizard.";
-                    case "paper":
-                        return "You win! Lizard eats paper.";
-                    case "scissors":
-                        return "You lose! Scissors decapitates lizard.";
-                    case "spock":
-                        return "You win! Lizard poisons Spock.";
-                    default:
-                        exit();
-                }
-            case "spock":
-                switch(computerChoice) {
-                    case "rock":
-                        return "You win! Spock vaporizes rock.";
-                    case "paper":
-                        return "You lose! Paper disproves Spock.";
-                    case "scissors":
-                        return "You win! Spock smashes scissors.";
-                    case "lizard":
-                        return "You lose! Lizard poisons Spock.";
-                    default:
-                        exit();
-                }
-            default:
+        
+        if (userChoice.equals("rock")) {
+            if (computerChoice.equals("paper")) {
+                computerScore++;
+                return "You lose! Paper covers rock.";
+            } else if (computerChoice.equals("scissors")) {
+                userScore++;
+                return "You win! Rock crushes scissors.";
+            } else if (computerChoice.equals("lizard")) {
+                userScore++;
+                return "You win! Rock crushes lizard.";
+            } else if (computerChoice.equals("spock")) {
+                computerScore++;
+                return "You lose! Spock vaporizes rock.";
+            } else {
                 exit();
+            }
+        } else if (userChoice.equals("paper")) {
+            if (computerChoice.equals("rock")) {
+                userScore++;
+                return "You win! Paper covers rock.";
+            } else if (computerChoice.equals("scissors")) {
+                computerScore++;
+                return "You lose! Scissors cuts paper.";
+            } else if (computerChoice.equals("lizard")) {
+                computerScore++;
+                return "You lose! Lizard eats paper.";
+            } else if (computerChoice.equals("spock")) {
+                userScore++;
+                return "You win! Paper disproves Spock.";
+            } else {
+                exit();
+            }
+        } else if (userChoice.equals("scissors")) {
+            if (computerChoice.equals("rock")) {
+                computerScore++;
+                return "You lose! Rock crushes scissors.";
+            } else if (computerChoice.equals("paper")) {
+                userScore++;
+                return "You win! Scissors cuts paper.";
+            } else if (computerChoice.equals("lizard")) {
+                userScore++;
+                return "You win! Scissors decapitates lizard.";
+            } else if (computerChoice.equals("spock")) {
+                computerScore++;
+                return "You lose! Spock smashes scissors.";
+            } else {
+                exit();
+            }
+        } else if (userChoice.equals("lizard")) {
+            if (computerChoice.equals("rock")) {
+                computerScore++;
+                return "You lose! Rock crushes lizard.";
+            } else if (computerChoice.equals("paper")) {
+                userScore++;
+                return "You win! Lizard eats paper.";
+            } else if (computerChoice.equals("scissors")) {
+                computerScore++;
+                return "You lose! Scissors decapitates lizard.";
+            } else if (computerChoice.equals("spock")) {
+                userScore++;
+                return "You win! Lizard poisons Spock.";
+            } else {
+                exit();
+            }
+        } else if (userChoice.equals("spock")) {
+            if (computerChoice.equals("rock")) {
+                userScore++;
+                return "You win! Spock vaporizes rock.";
+            } else if (computerChoice.equals("paper")) {
+                computerScore++;
+                return "You lose! Paper disproves Spock.";
+            } else if (computerChoice.equals("scissors")) {
+                userScore++;
+                return "You win! Spock smashes scissors.";
+            } else if (computerChoice.equals("lizard")) {
+                computerScore++;
+                return "You lose! Lizard poisons Spock.";
+            } else {
+                exit();
+            }
+        } else {
+            exit();
         }
+        
+        exit();
         return null;
     }
     
